5d4905ae913938276dd9491d5f09b1165a9fcf9c
Jason Co <jasonco@m-1784.lan>
2026-02-09 16:16:43 -0800

    final comments

--- tariff.cpp
+++ tariff.cpp
@@ -5,8 +5,103 @@ The main challenges that I faced for these functions were debugging
 and determining the best way too go about checking/implementing certain functionaltiy
 
 
-For the first 
+For the first function instead of using an array/for loop I opted to ue a while loop
 
+I initially struggled because I wanted to format the string in the right way by cpaitilizing to have a universal
+check for the country, getting a proper int for the number and parsing, however I realized
+that this was actually for the 2nd funciton.
+
+Once I realized htis, I was able to break it down into smaller parts with pseudocode.
+
+I knew that I needed to iterate over a string but did not know that hte stirng contained multiple
+of these coutnry codes, when realizing this Iknew that I needed "2 loops in a sense"
+Because I needed to pass over all these codes but evey time we encountered a new code, 
+we needed to "reset" what we were looking for
+
+As a result I made a temporary variable  called length to kepe track of these parementers
+for my pseudo code
+
+while current i index is less than the whole string
+    if the first 2 indexes are not valid letters then return false
+    c keeps track of the letters to get the NZ edge case capitalizes both so only hav eto check NZ vs nz etc
+    then check for how many numbers if its a 1 digit or 2 digit code ie 1 vs 99
+    we do this by checking against the next element vs the size of the stirng to see if its invalid
+    i set the default case to len - 1
+    then we check if it is a digit respecitevly
+    based on this modified length we then check the next letter for expectation
+    after we then index vy skipping a space based on if the whole stirn gis compelted to mvoe to the next code
+
+For the 2nd funciton I used a similar concept the mainc hallenges I had were formatting for example to get the countyr code number
+turn that string into an int and how to perform operations and store it
+
+I also notinced that to deal with pass by reference vs pass by value (copy)
+where we are directyl returning and modifying which helps with our test cases and the utility of the funciton
+
+I created temproary coutners to treat the upper or down and other caes in order to calcualte things like mean
+
+the above function used a similar pesuedo code 
+wioth a deafult length and checks if its cirrectly formatted first because no gauarantee the user runs it first
+
+while current i index is less than the whole string
+    handle 1 case see if its calid first
+
+    creating more temp variables than before
+    i still stands for index
+    size for size
+    c for country
+    length as a way to move between phrases
+
+    pct standards for percent
+    total and count are used to calculate mean percentage which is returned as pass by reference
+    these are used in a final calc at the end of the function
+
+    up and down are also used as coutners for a final calc respectively
+
+    we no longer need to check if the stirng is valid stince it passed the case but we can use some of the same logic
+        if the first 2 indexes are not valid letters then return false
+        c keeps track of the letters to get the NZ edge case capitalizes both so only hav eto check NZ vs nz etc
+        then check for how many numbers if its a 1 digit or 2 digit code ie 1 vs 99
+        we do this by checking against the next element vs the size of the stirng to see if its invalid
+        i set the default case to len - 1
+        then we check if it is a digit respecitevly
+        based on this modified length we then check the next letter for expectation
+        after we then index vy skipping a space based on if the whole stirn gis compelted to mvoe to the next code
+
+    pct = tariffData[i + 2] - '0'; //0-9 are consecutive in ascii
+    i had trouble at first because we cannot just multiple this or treat is as an int for our calculations
+    we need to first convert this to an int and can do so by this function since its onyl one mnumber at a time
+
+    we can then move over 1 decimal place if needed for the 2 digit "edge" case 
+    if (i + 3 < size && isdigit(tariffData[i + 3]))
+        {
+            //move decimal place over 1
+            pct = pct * 10 + (tariffData[i + 3] - '0');
+            len = 5; //increase length
+        }
+
+
+    after we check for the expectationc ase where 
+    if ((pct == 99 && (expect == 'H' || expect == 'h')) || (pct == 0  && (expect == 'L' || expect == 'l')))
+        {
+            return 2;
+        }
+    I decided to do an explicit check here
+
+    the way this is ordered as well helps us and is most logical for a decision tree
+    by having a known index with a known pattern, we are able to add more checkers if neede to the function
+    as we know what place we are in and cn bring up edge cases before proceeding
+
+    Finally the incrmeents and aclcs are performed a thte bottom
+    /for mean canculation at end
+        total += pct;
+        count++;
+        //onyl need to check the high case
+        if (expect == 'H' || expect == 'h')
+            up++;
+        else
+            down++;
+        //increment to next phrase
+        i += len;
 
 
 
@@ -98,6 +193,7 @@ bool isCorrectlyFormed(string tariffData)
     return true;
 }
 
+//function 2 :)
 int summarizeData(string tariffData, double& meanPercentage, int& numUp, int& numDown)
 {
 	// TODO: Correctly implement this function!!
--- tariff_test.cpp
+++ tariff_test.cpp
@@ -16,16 +16,22 @@ int main()
 	// Two sample test cases
 	    assert(isCorrectlyFormed("FR7HjP15lUS9Hin00H"));
         assert(!isCorrectlyFormed("ZZ7HjP15lNZ9Hin00H"));
+
+    //valid cases
+        assert(isCorrectlyFormed("us2h"));
+        assert(isCorrectlyFormed("us02h"));
         //no expectation
-        assert(isCorrectlyFormed("US2"));
+        assert(!isCorrectlyFormed("US2"));
         //too short
-        assert(isCorrectlyFormed("US"));
+        assert(!isCorrectlyFormed("US"));
+        //too long
+        assert(!isCorrectlyFormed("US11p78"));
         //3 digit
-        assert(isCorrectlyFormed("US100"));
+        assert(!isCorrectlyFormed("US100"));
         //wrong expectation
-        assert(isCorrectlyFormed("US11P"));
+        assert(!isCorrectlyFormed("US11P"));
         //non expected char
-        assert(isCorrectlyFormed("US!"));
+        assert(!isCorrectlyFormed("US!"));
 
 
         
@@ -36,15 +42,17 @@ int main()
 	// Two sample test cases
 
         // provided in spec
+        //return 1
         mean = -999; nUp = -999; nDown = -999; 
         assert(summarizeData("FR7HjP15lUS9Hin00H", mean, nUp, nDown) == 0  &&
         isNear(mean, 7.75)  &&  nUp == 3  &&  nDown == 1);
+        //return 1
         mean = -999; nUp = -999; nDown = -999; 
         assert(summarizeData("ZZ7HjP15lNZ9Hin00H", mean, nUp, nDown) == 1  &&
             mean == -999  &&  nUp == -999  &&  nDown == -999);
         //
 
-        // empty string
+        // empty string 0
         mean = -999; nUp = -999; nDown = -999; 
         assert(summarizeData("", mean, nUp, nDown) == 0  &&
         mean == 0  &&  nUp == 0 &&  nDown == 0);
@@ -57,6 +65,11 @@ int main()
         mean = -999; nUp = -999; nDown = -999; 
         assert(summarizeData("US0L", mean, nUp, nDown) == 2  &&
         mean == -999  &&  nUp == -999 &&  nDown == -999);
+
+        //regular case 0
+        mean = -999; nUp = -999; nDown = -999; 
+        assert(summarizeData("US7L", mean, nUp, nDown) == 0  &&
+        isNear(mean, 7.0)   &&  nUp == 0 &&  nDown == 1);
         
 
         cerr << "All tests succeeded" << endl;
